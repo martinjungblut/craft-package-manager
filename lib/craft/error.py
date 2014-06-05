5842eeee96d5432627df589492f54a989424d623
Martin Jungblut <martinjungblut@gmail.com>
2014-06-05 10:29:51 -0300

    Errors and warnings are now timed and coloured.

--- lib/craft/error.py
+++ lib/craft/error.py
@@ -1,15 +1,40 @@
-""" Interface for handling errors and warnings. """
+""" Handle errors and warnings. """
 
 # Standard library imports
 from sys import exit as sys_exit
+from time import localtime
+
+def _isotime():
+    """ Retrieve the current time as a string,
+    formatted according to the ISO-8601 standard. """
+
+    now = list(localtime()[:-3])
+
+    for each in range(1, len(now)):
+        if now[each] < 10:
+            now[each] = '0'+str(now[each])
+
+    return '{0}/{1}/{2} {3}:{4}:{5}'.format(now[0], now[1], now[2], now[3], now[4], now[5])
 
 def fatal(message, status):
     """ Informs the user a fatal error has occurred.
-    message is printed to the screen
-    status is returned as the system's exit status. """
-    print("Fatal error: "+message)
+
+    Paramaters
+        message
+            message to be printed before aborting.
+        status
+            returned as the system's exit status.
+    """
+
+    print('\033[91m'+_isotime()+' Fatal error: '+message+'\033[0m')
     sys_exit(status)
 
 def warning(message):
-    """ Informs the user about an unexpected event. """
-    print("Warning: "+message)
+    """ Informs the user about an unexpected event.
+
+    Paramaters
+        message
+            the event's description.
+    """
+
+    print('\033[93m'+_isotime()+' Warning: '+message+'\033[0m')
